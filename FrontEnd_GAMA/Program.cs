8e49c828fd8af87a3f264908d630c24c8e4771e8
jose.aurivan <aurivanfilho2013@gmail.com>
2023-06-27 21:02:35 -0300

    initial commit

---
+++ FrontEnd_GAMA/Program.cs
@@ -0,0 +1,32 @@
+using FrontEnd_GAMA.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<HttpClient>(sp => new HttpClient {BaseAddress = new Uri("http://localhost:5042") });
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
